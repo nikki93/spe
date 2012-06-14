648c8659de08769f7b4d7c24b0c407b3fade7977
nikki93 <s.nikhilesh@gmail.com>
2012-06-14 16:50:35 -0400

    Update premake4.lua file... Again

--- brushtest/premake4.lua
+++ brushtest/premake4.lua
@@ -164,68 +164,53 @@ includedirs {
     ofpath .. "/libs/openFrameworks/sound",
     ofpath .. "/libs/openFrameworks/gl",
     ofpath .. "/libs/openFrameworks/app",
-    ofpath .. "/libs/assimp/include",
-    ofpath .. "/libs/assimp/include/Compiler",
-    ofpath .. "/libs/cairo/include",
-    ofpath .. "/libs/cairo/include/cairo",
-    ofpath .. "/libs/cairo/include/libpng15",
-    ofpath .. "/libs/cairo/include/pixman-1",
-    ofpath .. "/libs/fmodex/include",
-    ofpath .. "/libs/FreeImage/include",
+
+    ofpath .. "/libs/glut/include",
+    ofpath .. "/libs/rtAudio/include",
+    ofpath .. "/libs/quicktime/include",
     ofpath .. "/libs/freetype/include",
     ofpath .. "/libs/freetype/include/freetype2",
-    ofpath .. "/libs/freetype/include/freetype2/freetype",
-    ofpath .. "/libs/freetype/include/freetype2/freetype/config",
-    ofpath .. "/libs/freetype/include/freetype2/freetype/internal",
-    ofpath .. "/libs/freetype/include/freetype2/freetype/internal/services",
+    ofpath .. "/libs/FreeImage/include",
+    ofpath .. "/libs/fmodex/include",
+    ofpath .. "/libs/videoInput/include",
+    ofpath .. "/libs/glu/include",
     ofpath .. "/libs/glew/include",
-    ofpath .. "/libs/glew/include/GL",
-    ofpath .. "/libs/kiss/include",
-    ofpath .. "/libs/poco/include",
-    ofpath .. "/libs/poco/include/Poco",
-    ofpath .. "/libs/poco/include/Poco/Util",
-    ofpath .. "/libs/poco/include/Poco/SAX",
-    ofpath .. "/libs/poco/include/Poco/Dynamic",
-    ofpath .. "/libs/poco/include/Poco/DOM",
-    ofpath .. "/libs/poco/include/Poco/Net",
-    ofpath .. "/libs/poco/include/Poco/XML",
-    ofpath .. "/libs/poco/include/CppUnit",
-    ofpath .. "/libs/portaudio/include",
-    ofpath .. "/libs/rtAudio/include",
-    ofpath .. "/libs/tess2/include"
+    ofpath .. "/libs/tess2/include",
+    ofpath .. "/libs/cairo/include/cairo",
+    ofpath .. "/libs/Poco/include",
+    ofpath .. "/addons",
 }
 
 libdirs  {
-    ofpath .. "/libs/openFrameworksCompiled/lib/linux64",
-    ofpath .. "/libs/freetype/lib/linux64",
-    ofpath .. "/libs/kiss/lib/linux64",
-    ofpath .. "/libs/portaudio/lib/linux64",
-    ofpath .. "/libs/rtAudio/lib/linux64",
-    ofpath .. "/libs/tess2/lib/linux64",
-    ofpath .. "/libs/poco/lib/linux64",
-    ofpath .. "/libs/poco/lib/linux64",
-    ofpath .. "/libs/poco/lib/linux64",
-    ofpath .. "/libs/poco/lib/linux64",
-    ofpath .. "/libs/fmodex/lib/linux64",
-    ofpath .. "/libs/freetype/lib/linux64",
-    ofpath .. "/libs/kiss/lib/linux64",
-    ofpath .. "/libs/openFrameworksCompiled/lib/linux64",
-    ofpath .. "/libs/poco/lib/linux64",
-    ofpath .. "/libs/portaudio/lib/linux64",
-    ofpath .. "/libs/rtAudio/lib/linux64",
-    ofpath .. "/libs/tess2/lib/linux64"
+    ofpath .. "/libs/openFrameworksCompiled/lib/vs2010",
+
+    ofpath .. "/libs/glut/lib/vs2010",
+    ofpath .. "/libs/rtAudio/lib/vs2010",
+    ofpath .. "/libs/FreeImage/lib/vs2010",
+    ofpath .. "/libs/freetype/lib/vs2010",
+    ofpath .. "/libs/quicktime/lib/vs2010",
+    ofpath .. "/libs/fmodex/lib/vs2010",
+    ofpath .. "/libs/videoInput/lib/vs2010",
+    ofpath .. "/libs/cairo/lib/vs2010",
+    ofpath .. "/libs/glu/lib/vs2010",
+    ofpath .. "/libs/glew/lib/vs2010",
+    ofpath .. "/libs/Poco/lib/vs2010",
+    ofpath .. "/libs/tess2/lib/vs2010"
 }
 
 links  { 
-    "openFrameworks", "kiss", "tess2", "PocoNet", "PocoXML", "PocoUtil", 
-    "PocoFoundation", "mpg123", "GLEW", "cairo", "glut", "GL", "openal", "sndfile", 
-    "freeimage", "GLU", "pthread"
+    "cairo-static", "pixman-1", "msimg32", "OpenGL32", "GLu32", "kernel32", "setupapi",
+    "Vfw32", "comctl32", "glut32", "rtAudioD", "videoInput", "libfreetype", "FreeImage",
+    "qtmlClient", "dsound", "user32", "gdi32", "winspool", "comdlg32", "advapi32",
+    "shell32", "ole32", "oleaut32", "uuid", "glew32s", "fmodex_vc", "glu32",
+    "PocoFoundationmdd", "PocoNetmdd", "PocoUtilmdd", "PocoXMLmdd", "Ws2_32", "tess2",
+    "odbc32", "odbccp32"
 }
 
 -- Build options ----------------------------------------------------------------------------
 
 buildoptions "/MP"
-linkoptions { "/NODEFAULTLIB:libcpmt.lib", "/NODEFAULTLIB:LIBCMT", "/SUBSYSTEM:WINDOWS" }
+linkoptions { "/SUBSYSTEM:WINDOWS" }
 
 -- Configurations ---------------------------------------------------------------------------
 
