38f11586de5f55e41cf4000d5d471424399c559a
ringosake <cissyzed@gmail.com>
2012-06-22 17:23:25 -0400

    add empty project

---
+++ empty/src/App.h
@@ -0,0 +1,30 @@
+#ifndef __APP_H__
+#define __APP_H__
+
+#include <ofBaseApp.h>
+#include <ofCamera.h>
+#include <ofLight.h>
+
+#include "Ball.h"
+
+class App : 
+    public ofBaseApp
+{
+    public:
+        App();
+
+        void setup();
+        void update();
+        void draw();
+        void exit();
+
+        void keyPressed(int key);
+        void keyReleased(int key);
+        void mouseMoved(int x, int y );
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void windowResized(int w, int h);
+};
+
+#endif
