614ceb98b9e73cd5a907b6e1942384cfa7f8ba23
unknown <cissyz@PC5234.pu.win.princeton.edu>
2012-06-28 18:16:39 -0400

    wrote some brush size code. bad coarse layer right now!!!!

--- brushtest/src/App.h
+++ brushtest/src/App.h
@@ -1,46 +1,37 @@
 #ifndef __APP_H__
 #define __APP_H__
-
 #include <ofBaseApp.h>
 #include <ofImage.h>
-
 #include <ofFbo.h>
 #include <ofShader.h>
-
 #include "Brush.h"
 #include "Field.h"
 #include "Palette.h"
-
 class App : 
     public ofBaseApp
 {
     protected:
         Field _forceField;
         ofImage _potential;
-
+        ofImage _edges; // temp
+        std::vector<std::vector<ofVec2f>> _brushSizes;
         ofImage _color;
-		Palette *_palette;
-
+Palette *_palette;
         typedef std::vector<Brush *> BrushList;
         BrushList _brushes;
-
         ofVec2f _mousePrev;
         float _fieldRedrawTimer;
         bool _fieldRedrawCounting;
         ofFbo _fbo;
         ofShader _shader;
-
         const float *getRadii(int levels, float minRadius, float maxRadius);
        
-
     public:
         App() : _forceField(1024, 768) { }
-
         void setup();
         void update();
         void draw();
         void clear();
-
         void keyPressed(int key);
         void keyReleased(int key) { }
         void mouseMoved(int x, int y );
@@ -48,8 +39,6 @@ class App :
         void mousePressed(int x, int y, int button);
         void mouseReleased(int x, int y, int button);
         void windowResized(int w, int h) { }
-
         void createBrushes();
 };
-
 #endif
